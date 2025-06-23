fd118daab8af3c1fdd5a65f60553e92cc790226e
Jonas Bonaudet <jonas.bonnaudet@gmail.com>
2025-06-23 18:35:02 +0200

    screen added

--- test.cpp
+++ test.cpp
@@ -1,11 +1,30 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+
+#include "pico-ssd1306/ssd1306.h"
+#include "pico-ssd1306/textRenderer/TextRenderer.h"
 
 #define TRIGGER_PIN 2
 #define ECHO_PIN 3
 #define TIMEOUT_US 50000 // 50 ms timeout
 
-#define num_smooth 100
+#define I2C_PORT i2c_default
+#define I2C_PIN_SDA PICO_DEFAULT_I2C_SDA_PIN
+#define I2C_PIN_SCL PICO_DEFAULT_I2C_SCL_PIN
+
+unsigned int num_smooth = 10;
+void read_smooth()
+{
+    while (1)
+    {
+        int in = -1;
+        scanf("%d", &in);
+        num_smooth = in;
+        printf("num_smooth is %d\n", in);
+    }
+}
 
 double measure_distance()
 {
@@ -43,8 +62,8 @@ double measure_distance()
 
     echo_end = get_absolute_time();
     int64_t pulse_duration = absolute_time_diff_us(echo_start, echo_end);
-    float distance_cm = pulse_duration / 58.0f;
-    return distance_cm;
+    float distance_mm = pulse_duration / 5.80f;
+    return distance_mm;
 }
 
 int main()
@@ -61,15 +80,31 @@ int main()
     gpio_init(ECHO_PIN);
     gpio_set_dir(ECHO_PIN, GPIO_IN);
 
+    i2c_init(I2C_PORT, 1000000); // Use i2c port with baud rate of 1Mhz
+    // // Set pins for I2C operation
+    gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_PIN_SDA);
+    gpio_pull_up(I2C_PIN_SCL);
+
+    // // Create a new display object
+    pico_ssd1306::SSD1306 display = pico_ssd1306::SSD1306(I2C_PORT, 0x3C, pico_ssd1306::Size::W128xH32); // 0x3D for 128x64
+
+    multicore_launch_core1(read_smooth);
+
+    double d = 150.;
     while (true)
     {
-        double d = 0.;
 
-        for (int i = 0; i < num_smooth; i++)
-        {
-            d += measure_distance() / num_smooth;
-        }
+        // for (int i = 0; i < num_smooth; i++)
+        // {
+        d = measure_distance() / num_smooth + d * (num_smooth - 1) / num_smooth;
+        // }
 
-        printf("Distance: %.2f cm\n", d);
+        display.clear();
+        char str[20];
+        sprintf(str, "%.0f mm", d);
+        drawText(&display, font_16x32, str, 0, 0);
+        display.sendBuffer(); // Send buffer to device and show on screen
     }
 }
